676c7646a0ad8abcb073f48e2fe4e5b8a713c863
yonnie <70378641+minuut@users.noreply.github.com>
2023-12-28 19:09:54 +0100

    fixed edit logic for loanbikes

--- app/Filament/Resources/AppointmentResource/Pages/EditAppointment.php
+++ app/Filament/Resources/AppointmentResource/Pages/EditAppointment.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\AppointmentResource\Pages;
 use Filament\Actions;
 use App\Models\LoanBike;
 use App\Models\Appointment;
+use App\Enums\LoanBikeStatus;
 use Filament\Resources\Pages\EditRecord;
 use App\Filament\Resources\AppointmentResource;
 
@@ -36,6 +37,31 @@ class EditAppointment extends EditRecord
             $data['date'] = $this->record->date;
             $data['mechanic'] = $this->record->slot->schedule->owner_id;
             $data['loan_bike_id'] = $this->record->loan_bike_id;
+        }
+
+        return $data;
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        // Check if the loan_bike_id is being updated
+        if ($this->record instanceof Appointment && isset($data['loan_bike_id']) && $data['loan_bike_id'] !== $this->record->loan_bike_id) {
+            if ($this->record->loan_bike_id) {
+                $previousLoanBike = LoanBike::find($this->record->loan_bike_id);
+                if ($previousLoanBike) {
+                    $previousLoanBike->status = LoanBikeStatus::Available;
+                    $previousLoanBike->save();
+                }
+            }
+
+            // Updating the status of the newly selected LoanBike to 'rented_out'
+            if ($data['loan_bike_id']) {
+                $newLoanBike = LoanBike::find($data['loan_bike_id']);
+                if ($newLoanBike) {
+                    $newLoanBike->status = LoanBikeStatus::RentedOut;
+                    $newLoanBike->save();
+                }
+            }
         }
 
         return $data;
