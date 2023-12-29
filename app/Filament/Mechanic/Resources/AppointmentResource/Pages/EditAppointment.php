4898caf06e9b8c0f53fc0ec29dd9cf8cb5b4a660
yonnie <70378641+minuut@users.noreply.github.com>
2023-12-29 21:33:01 +0100

    Update EditAppointment.php

--- app/Filament/Mechanic/Resources/AppointmentResource/Pages/EditAppointment.php
+++ app/Filament/Mechanic/Resources/AppointmentResource/Pages/EditAppointment.php
@@ -4,10 +4,10 @@ namespace App\Filament\Mechanic\Resources\AppointmentResource\Pages;
 
 use Filament\Actions;
 use App\Models\LoanBike;
-use App\Models\Appointment;
 use App\Enums\LoanBikeStatus;
 use Filament\Resources\Pages\EditRecord;
 use App\Filament\Mechanic\Resources\AppointmentResource;
+use App\Models\Appointment;
 
 class EditAppointment extends EditRecord
 {
@@ -42,22 +42,17 @@ class EditAppointment extends EditRecord
         // original model data to get the old loan_bike_id
         $oldLoanBikeId = $this->record->getOriginal('loan_bike_id');
 
-        // Check if 'loan_bike_id' key exists in $data and has changed
-        if (array_key_exists('loan_bike_id', $data) && $data['loan_bike_id'] !== $oldLoanBikeId) {
+        if ($data['loan_bike_id'] !== $oldLoanBikeId) {
             $previousLoanBike = LoanBike::find($oldLoanBikeId);
+
             if ($previousLoanBike) {
                 $previousLoanBike->status = LoanBikeStatus::Available;
                 $previousLoanBike->save();
             }
         }
 
-        // Check if 'loan_bike_id' is not set or is null
-        if (!isset($data['loan_bike_id']) || $data['loan_bike_id'] === null) {
-            $data['loan_bike_id'] = null;
-        }
-
-        // Check if 'loan_bike_id' is set and not equal to the current loan_bike_id
-        if (isset($data['loan_bike_id']) && $data['loan_bike_id'] !== $this->record->loan_bike_id) {
+        // Find the new LoanBike and update its status to RentedOut
+        if ($data['loan_bike_id']) {
             $newLoanBike = LoanBike::find($data['loan_bike_id']);
             if ($newLoanBike) {
                 $newLoanBike->status = LoanBikeStatus::RentedOut;
