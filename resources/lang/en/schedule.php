5a4d3e5c06bf8da042db1a6217684bf4dd8c90e6
Yonnie <70378641+minuut@users.noreply.github.com>
2024-06-19 17:08:08 +0200

    schedules

---
+++ resources/lang/en/schedule.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'all_schedules' => 'All Schedules',
+    'days' => [
+        'sunday' => 'Sunday',
+        'monday' => 'Monday',
+        'tuesday' => 'Tuesday',
+        'wednesday' => 'Wednesday',
+        'thursday' => 'Thursday',
+        'friday' => 'Friday',
+        'saturday' => 'Saturday',
+    ],
+];
---
+++ resources/lang/nl/schedule.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'all_schedules' => 'Alle roosters',
+    'days' => [
+        'sunday' => 'Zondag',
+        'monday' => 'Maandag',
+        'tuesday' => 'Dinsdag',
+        'wednesday' => 'Woensdag',
+        'thursday' => 'Donderdag',
+        'friday' => 'Vrijdag',
+        'saturday' => 'Zaterdag',
+    ],
+];
